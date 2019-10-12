fec411132c1f5433b1eb0df6b14df45390803817
Kudzai Huni <sean2kay@gmail.com>
2019-10-12 13:28:17 +0200

    - fixed the U.I date issues.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,8 +10,6 @@ import {RollingLogsModel} from "../../models/rolling-logs.model";
 })
 export class HomeComponent implements OnInit {
     private inView: Boolean = true;
-    private time: Date = new Date(2019, 8, 21, 19, 32, 12);
-    private now: Date = new Date();
     private fDate: string = 'MM/dd/yyyy HH:mm:ss';
     private locale: string = 'en-ZA';
 
@@ -94,4 +92,58 @@ export class HomeComponent implements OnInit {
         return aDate;
     }
 
+    dateSuperscriptSuffix(day: number) {
+        if (day > 3 && day < 21) return 'th';
+        switch (day % 10) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
+
+    sliceDate(dateStr: string) {
+        let x: Map<string, any> = new Map<any, any>();
+        let dateAndTimeArr = dateStr.split(' ');
+        let dateArr = dateAndTimeArr[0].split('/', 3);
+
+        x.set('day', dateArr[1]);
+        x.set('month', dateArr[0]);
+        x.set('year', dateArr[2]);
+        return x;
+    }
+
+    monthOfTheYear(cardinalYear: string) {
+        let fYear = parseInt(cardinalYear);
+        switch (fYear) {
+            case 1:
+                return 'January';
+            case 2:
+                return 'February';
+            case 3:
+                return 'March';
+            case 4:
+                return 'April';
+            case 5:
+                return 'May';
+            case 6:
+                return 'June';
+            case 7:
+                return 'July';
+            case 8:
+                return 'August';
+            case 9:
+                return 'September';
+            case 10:
+                return 'October';
+            case 11:
+                return 'November';
+            default:
+                return 'December';
+        }
+    }
 }
